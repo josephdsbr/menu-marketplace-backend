b2089b0e99893d96ef2c05a3c378d551300283d5
josephdsbr <josephdsbr@gmail.com>
2021-01-13 12:29:19 -0300

    finalizando configuracoes de lint e teste

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["prettier", "@typescript-eslint", "simple-import-sort"],
+  ignorePatterns: "build",
+  rules: {
+    "prettier/prettier": "error",
+    "simple-import-sort/sort": "warn",
+  },
+};
